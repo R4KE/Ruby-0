1cebaf84a8bc2a2bd1826611b38b3cc17bf2e507
Jurre de Vries <31882165+R4KE@users.noreply.github.com>
2019-04-06 12:08:17 +0200

    Working on classes/objects

--- Hoofdstuk4/opdracht4a.rb
+++ Hoofdstuk4/opdracht4a.rb
@@ -1,1 +1,22 @@
-#komt nog
+class Book
+  attr_accessor :title, :author, :content, :currentpage
+
+  def initialize(author, title, content)
+    @author = author
+    @title = title
+    @content = content
+    @currentpage = 1
+  end
+
+  def flip_page
+    @currentpage = @currentpage + 1
+  end
+
+  def words
+    @content.split(" ")
+  end
+end
+
+book1 = Book.new("Martens en de Vries", "Prisma van de handvuurwapens", "Het begin van de ontwikkeling van het vuurwapen...")
+puts book1.title
+puts book1.words
---
+++ Hoofdstuk4/opdracht4b.rb
@@ -0,0 +1,5 @@
+require "date"
+
+date = Date.new(2014, 2, 3)
+date.wday # => 2
+date.tuesday? # => true
